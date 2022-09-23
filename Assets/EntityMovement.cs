770697f64917f9c3edb355fce0ba806eadc148a0
Red <lebruitdered@gmail.com>
2022-09-23 10:36:34 +0200

    Huge import

---
+++ Assets/EntityMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] float _speed;
+
+    Vector3 _direction;
+
+    public Vector3 Direction
+    {
+        get => _direction;
+        set => _direction = (value).normalized;
+    }
+
+    void FixedUpdate()
+    {
+
+        var calculatedDirection = (_direction * _speed * Time.fixedDeltaTime);
+
+        _rb.MovePosition(_rb.transform.position + calculatedDirection);
+
+    }
+
+
+}
